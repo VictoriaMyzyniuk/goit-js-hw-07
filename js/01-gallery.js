ca9f554b50e0ba48a66249da765f3217e3850f1c
Victoria Myzynyuk <mezochka93@gmail.com>
2022-07-23 22:20:48 +0300

    complete task one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imgMarkup = addGalleryElements(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgMarkup);
+galleryEl.addEventListener("click", onGalleryClick);
+
+let bigImgEl;
+
+function addGalleryElements(elements) {
+  return elements
+    .map((el) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function notOpenNewPage(evt) {
+  evt.preventDefault();
+}
+
+function onGalleryClick(evt) {
+  notOpenNewPage(evt);
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  onModalOpen(evt);
+}
+
+function onEscapePress(evt) {
+  const isEscKey = evt.code === "Escape";
+  if (isEscKey) {
+    onModalClose();
+  }
+}
+
+function onModalOpen(evt) {
+  bigImgEl = basicLightbox.create(
+    `
+       <img src="${evt.target.dataset.source}">
+  `,
+    {
+      onClose: () => {
+        window.removeEventListener("keydown", onEscapePress);
+      },
+    }
+  );
+
+  bigImgEl.show();
+  window.addEventListener("keydown", onEscapePress);
+}
+
+function onModalClose() {
+  bigImgEl.close();
+  window.removeEventListener("keydown", onEscapePress);
+}
