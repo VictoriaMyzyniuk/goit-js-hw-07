fd1b477dc3eb77bf8f7f47e84bfc4513e6bd69f2
Victoria Myzynyuk <mezochka93@gmail.com>
2022-07-24 19:09:21 +0300

    add task two

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imgMarkup = addGalleryElements(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgMarkup);
+galleryEl.addEventListener("click", notOpenNewPage);
+
+function addGalleryElements(elements) {
+  return elements
+    .map((el) => {
+      return `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+              </a>`;
+    })
+    .join("");
+}
+
+function notOpenNewPage(evt) {
+  evt.preventDefault();
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
